064a673f5d0ea18e4ec5bf84ff38fe61ba74c315
Abid Hasan <144882936+abid450@users.noreply.github.com>
2023-09-29 11:55:49 +0600

    I have a upload programming c file

---
+++ main.c
@@ -0,0 +1,21 @@
+
+
+  // DEferents ADD OF Number-------------------)
+
+    
+    /* #include<stdio.h>
+
+     int main(){
+
+        int num1 = 40;
+        int num2 = 70;
+        int num3 = 90;
+        int num4 = 60;
+        int num5 = 80;
+
+        printf("Numbers are %d , %d, %d,%d\n",num1,num2,num3,num4,num5);
+     } 
+
+    */
+
+
